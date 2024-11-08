fb03acf9c6d1c4e2a917701124bc90ad066728bc
vijayloganathan <vijayloganathan2002@gmail.com>
2024-11-08 10:52:24 +0530

    8-11-24

--- src/batchprogram/daily-jobs.ts
+++ src/batchprogram/daily-jobs.ts
@@ -2,12 +2,14 @@ import cron from "node-cron";
 import { BatchRepository } from "../api/batch/birthday-repository";
 
 // Schedule the batch job to run at 9:00 AM daily
-cron.schedule("0 9 * * *", async () => {
+cron.schedule("0 10 * * *", async () => {
   console.log("Running daily batch job at 9:00 AM");
 
   try {
-    const batchRepo = new BatchRepository();
-    await batchRepo.BirthdayRepositoryV1();
+    const batchRepo1 = new BatchRepository();
+    await batchRepo1.BirthdayRepositoryV1();
+    const batchRepo2 = new BatchRepository();
+    await batchRepo2.WeedingWishRepositoryV1();
     console.log("Daily batch job completed successfully");
   } catch (error) {
     console.error("Error in daily batch job:", error);

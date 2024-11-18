f62f49507d02e6689a536a36456a0f9c8fc78b3b
vijayloganathan <vijayloganathan2002@gmail.com>
2024-11-18 12:05:07 +0530

    Versel Depoye

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,8 @@ const init = async () => {
             "http://localhost:5173",
             "http://43.204.98.66",
             "http://65.2.69.27",
+            "https://testinguserpage.vercel.app/",
+            "https://ublis-landing-testing.vercel.app/",
           ], // Allowed origins
           headers: ["Accept", "Authorization", "Content-Type", "If-None-Match"], // Allowed headers
           exposedHeaders: ["WWW-Authenticate", "Server-Authorization"], // Exposed headers
